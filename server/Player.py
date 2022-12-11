ae64f405d93990f35031a6eba84a94cdf3632a41
SoNgSeUNgMiN96 <87978544+SoNgSeUNgMiN96@users.noreply.github.com>
2022-12-11 21:48:14 +0900

    Update Player.py

--- server/Player.py
+++ server/Player.py
@@ -1,6 +1,5 @@
 class Player:
     def __init__(self, connection, room, playerNumber):
-        self.color = 0
         self.connection = connection
         self.room = room
         self.playerNumber = playerNumber
